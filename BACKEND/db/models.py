cb6ed1b3edf799847b49fb5a05dfb6f1da54170c
qbr0q <qbroq@vk.com>
2025-03-21 11:01:28 +0300

    add models, tests, req. new endpoint, fix requests

--- BACKEND/db/models.py
+++ BACKEND/db/models.py
@@ -1,5 +1,6 @@
 from sqlmodel import (SQLModel, Field,
                       create_engine, Relationship)
+from sqlalchemy import JSON
 from datetime import datetime
 from typing import List
 
@@ -10,7 +11,7 @@ class Company(SQLModel, table=True):
     region: str
     description: str
     address: str
-    deleted: bool = Field(default=0)
+    deleted: bool = Field(default=False)
     vacancies: List['Vacancy'] = Relationship(back_populates='company')
 
 
@@ -22,9 +23,43 @@ class Vacancy(SQLModel, table=True):
     duration: str
     currency: str
     isCalling: bool
-    deleted: bool = Field(default=0)
+    deleted: bool = Field(default=False)
     company_id: int = Field(foreign_key='company.id')
     company: Company = Relationship(back_populates='vacancies')
+    vacancy_info: List['VacancyInfo'] = Relationship(back_populates='vacancy')
+
+
+class VacancyInfo(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    jobType: List[str] = Field(default=[], sa_type=JSON)
+    jobDuties: List[str] = Field(default=[], sa_type=JSON)
+    candidateRequirements: List[str] = Field(default=[], sa_type=JSON)
+    jobDescription: str
+    deleted: bool = Field(default=False)
+    vacancy_id: int = Field(foreign_key='vacancy.id')
+    vacancy: Vacancy = Relationship(back_populates='vacancy_info')
+
+
+class JobTypes(SQLModel, table=True):
+    __tablename__ = 'rbJobTypes'
+
+    id: int = Field(default=None, primary_key=True)
+    typeName: str
+
+
+class Duration(SQLModel, table=True):
+    __tablename__ = 'rbDuration'
+
+    id: int = Field(default=None, primary_key=True)
+    durationName: str
+
+
+class Currency(SQLModel, table=True):
+    __tablename__ = 'rbCurrency'
+
+    id: int = Field(default=None, primary_key=True)
+    currencyName: str
+    currencySymbol: str
 
 
 engine = create_engine("mysql+pymysql://dbuser:dbpassword@localhost:3306/dpmdb")
--- BACKEND/main.py
+++ BACKEND/main.py
@@ -1,19 +1,46 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 from config import HOST, PORT
-from db.models import Company, engine
+from db.models import engine, Company, Vacancy
+
 
 app = FastAPI()
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
 
 
 @app.get('/')
 async def test():
     print(1)
     with Session(engine) as session:
-        test = session.get(Company, 1)
-        print(test.name) # тест подключения к бд
+        test = session.get(Vacancy, 1)
+        return test.company # тест подключения к бд
+
+@app.get('/allVacancy')
+async def get_all_vacancy():
+    with Session(engine) as session:
+        pass
+    all_vacancy = session.exec(select(Vacancy).where(
+        Vacancy.deleted == False)
+    ).all()
+    return all_vacancy
+
+
+@app.get('/vacancy/{vacancy_id}')
+async def getVacancyInfo(vacancy_id: int):
+    with Session(engine) as session:
+        vacancy_info = session.get(Vacancy, vacancy_id)
+        return vacancy_info
 
 
 if __name__ == "__main__":
